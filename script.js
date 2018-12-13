c3eedf0dca2364b5b0e6fb47b96257b4380c8be8
Daniel <workkd97@gmail.com>
2018-12-13 20:28:51 +0200

    [*] Some unnecessary changes in temporary code;

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 
-var cardNumRegExp = /\/(\d{5,9})\?/gi;
+var cardNumRegExp = /\/(\d{16})\?/gi;
 
-document.addEventListener("DOMContentLoaded", function(e) {
-  var amount = 0;
-  var currency = 'EUR';
+document.addEventListener("DOMContentLoaded", function(event) {
+  var amount;
+  var currency;
   var urlParams = new URLSearchParams(window.location.search);
   amount = parseFloat(urlParams.get("amount"));
   console.log('amount is ', urlParams.get("amount"));
   currency = urlParams.get("currency");
-  console.log('Requested sum is ',amount, ' ', currency);
-  console.log(window.location.pathname)
-  // console.log(window.location.search.match(/\d{16}?/gi));
+  console.log('Requested sum is ', amount, ' ', currency);
+  // console.log(window.location.pathname.match(cardNumRegExp));
 });
